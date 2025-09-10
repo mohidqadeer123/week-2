6bb0fefd64277533c1a2068db86e4597abab312d
mohidqadeer123 <moqadeer@iu.edu>
2025-09-11 03:47:06 +0500

    Week 2

--- apputil.py
+++ apputil.py
@@ -4,10 +4,10 @@ import numpy as np
 # update/add code below ...
 Exercise 1
 def ways(n):
-    """
+    '''
     This functions returns the number of ways you can change n cents
     using only pennies (1 cent) and nickels (5 cents).
-    """
+    '''
     return int((n // 5) + 1)
 
 # Test cases
@@ -19,9 +19,9 @@ print(ways(0))   # 1
 
 Optional Variation
 def ways(cents, coin_types=[1, 5]):
-    """
+    '''
     This function returns the number of ways to make 'cents' using the given coin_types.
-    """
+    '''
     dp = [0] * (cents + 1)
     dp[0] = 1  # 1 way to make 0 cents
 
@@ -41,26 +41,25 @@ names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
 scores = np.array([99, 71, 85, 62, 91])
 
 def lowest_score(names, scores):
-    """
+    '''
     Returns the name of the student alongwith the lowest score.
-    """
+    '''
     # index of lowest score
     idx = np.argmin(scores)  
     return str(names[idx]), int(scores[idx])
     
 # Test Case
-print(lowest_score(names, scores))  
-# ('Mauve', 62)
+print(lowest_score(names, scores))  # gives ('Mauve', 62)
 
 Part 2
 
 def sort_names(names, scores):
-    """
+    '''
     Returns the names of students sorted by descending test score.
-    """
-    idx = np.argsort(scores)[::-1]   # indices for descending sort
+    '''
+    idx = np.argsort(scores)[::-1]   # indices for descending order
     return names[idx]                # apply ordering to names
 
 # Test Case
-print("Sorted by scores:", sort_names(names, scores)) 
+print("Sorted names by scores:", sort_names(names, scores)) 
 
